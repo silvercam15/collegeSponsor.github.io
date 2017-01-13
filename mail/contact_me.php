9280122fa539219dae16973fb6eabd39ef829680
silvercam15 <silviakcamara@gmail.com>
2017-01-12 19:47:31 -0600

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,9 @@
 <?php
+
+error_reporting(-1);
+ini_set('display_errors', 'On');
+set_error_handler("var_dump");
+
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
